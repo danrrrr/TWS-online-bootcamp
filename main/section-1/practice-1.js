ce649be8b3e17262bdd4dfbc0e8f85d4c4f5c09a
hudan <8518128599@qq.com>
2017-07-28 14:27:19 +0000

    optmise implementation.

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,13 +1,5 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  let result = new Array();
-    for(let i = 0; i < collectionA.length; i++){
-        for(let j = 0; j < collectionB.length; j++){
-            if(collectionA[i] == collectionB[j]){
-                result.push(collectionA[i]);
-            }
-        }
-    }
-  return result;
-}
+  return [... new Set(collectionA.filter(ele => collectionB.indexOf(ele) !== -1))];
+};
