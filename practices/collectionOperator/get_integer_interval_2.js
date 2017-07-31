af751eb760e6938f9470d6769da2fd644b64f9d0
Kai Hu <kwrongkey@gmail.com>
2017-07-31 22:24:52 +0800

    optimized get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,21 +1,10 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  // const arr = number_a % 2 === 0 ? [number_a] : [];
-  // if (number_a === number_b) {
-  //   return arr;
-  // }
-  // const offset = number_a < number_b ? 1 : -1;
-  // return [ ...arr, ...get_integer_interval_2(number_a + offset, number_b)];
-  let result = [];
-    let max = number_a > number_b ? number_a : number_b;
-    let min = number_a < number_b ? number_a : number_b;
-    for(let i = min; i <= max; i++){
-     if(i % 2 === 0){
-          result.push(i);
-      }
-  }
-	return number_a > number_b ? result.reverse() : result;
+  const arr = number_a % 2 === 0 ? [number_a] : [];
+  if(number_a === number_b) return arr;
+  const offset = number_a < number_b ? 1 : -1;
+  return [...arr, ...get_integer_interval_2(number_a + offset, number_b)];
 }
 
 
