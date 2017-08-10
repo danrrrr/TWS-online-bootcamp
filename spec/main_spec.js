a46e763796404c616e82e3a8d6d41041ffea5d8f
danr <851812859@qq.com>
2017-08-10 10:43:15 +0800

    task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -12,20 +12,39 @@ var main = require("../lib/main.js");
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("0 bottle", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(0);
+        var expect_string = `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+    it("1 bottle", function(){
+        var result = main(1);
+        var expect_string = `1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
 
         expect(expect_string).to.equal(result);
     });
+    it("3 bottles", function(){
+        let result = main(3);
+        var expect_string = `3 bottle of beer on the wall, 3 bottle of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+2 bottle of beer on the wall, 2 bottle of beer.
+Take one down and pass it around, 1 bottles of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+         expect(expect_string).to.equal(result);
+    });
+        it("99 bottles", function(){
+            let result = main(200);
+            console.log(result);
+        })
+   
 });
